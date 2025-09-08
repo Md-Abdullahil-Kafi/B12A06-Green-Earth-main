280359e37b7f75438c74a1954b7f5d84e6c20c02
Md. Abdullahil Kafi <ahkafi2003@gmail.com>
2025-09-08 19:43:11 +0600

    modal section complete

---
+++ main.js
@@ -0,0 +1,106 @@
+const spinner = document.getElementById('spinner');
+
+// Load Category
+const categoryLoad = ()=>{
+    fetch("https://openapi.programming-hero.com/api/categories")
+    .then(res=> res.json())
+    .then(data=> loadCategories(data.categories));
+}
+const loadCategories=(categories)=>{
+    const catSection = document.getElementById('categories');
+    catSection.innerHTML = "";
+    for(const category of categories){
+        const newLi = document.createElement('li');
+        newLi.innerHTML = `<button id="categoryBtn${category.id}" onclick="loadTrees(${category.id})" class="bg-gray-400 py-1 px-3 text-white rounded-sm hover:bg-green-700 cursor-pointer w-full btn categoryBtn">${category.category_name}</button>`;
+        catSection.append(newLi);
+    }
+}
+categoryLoad();
+
+
+
+
+// Load Plants
+const defaultPlants = ()=>{
+    fetch("https://openapi.programming-hero.com/api/plants")
+    .then(res => res.json())
+    .then(data => loadPlants(data.plants))
+}
+// Load Trees
+const loadTrees = (id)=>{
+    const cardContainer = document.getElementById('cardContainer');
+    cardContainer.innerHTML='';
+    spinner.classList.remove('hidden');
+    fetch(`https://openapi.programming-hero.com/api/category/${id}`)
+    .then(res => res.json())
+    .then(data => {
+        const categoryBtn = document.getElementById(`categoryBtn${id}`)
+        const allBtns = document.getElementsByClassName('categoryBtn');
+        for(const btn of allBtns){
+            btn.classList.remove('active')
+        }
+        categoryBtn.classList.add('active');
+        // console.log(categoryBtn)
+        loadPlants(data.plants)
+      
+    })
+    
+}
+
+// Card Details
+
+const cardDetails = async(id)=>{
+    const url= `https://openapi.programming-hero.com/api/plant/${id}`
+    const res = await fetch(url) 
+    const details = await res.json();
+    detailView(details.plants);
+}
+
+const detailView = (card)=>{
+    const modalBox = document.getElementById('modalBox');
+    modalBox.innerHTML = `
+    <div class="card bg-white p-4 text-start space-y-3">
+                            <img class="mx-auto rounded-lg  aspect-3/2 object-cover" src="${card.image}" alt="Card Image">
+                            <h1 onclick="cardDetails(${card.id})" class="font-bold text-start">${card.name}</h1>
+                            <p class="opacity-80 text-[12px]">${card.description}</p>
+                            <div class="price">
+                            <div class="type flex justify-between">
+                                <p class="py-1 px-3 font-semibold bg-[#DCFCE7] text-[#15803D] rounded-full">${card.category}</p>
+                                <p class="font-semibold"><i class="fa-solid fa-bangladeshi-taka-sign"></i><span>${card.price}</span></p>
+                            </div>
+                            </div>
+                            <div class="addToCart">
+                                <button onclick="my_modal_2.showModal()" class="bg-[#15803D] btn font-bold text-center rounded-full w-full  text-white">Add To Cart</button>
+                            </div>
+                            </div>
+    `
+    my_modal_2.showModal();
+}
+
+const loadPlants = (plants) => {
+    const cardContainer = document.getElementById('cardContainer');
+    cardContainer.innerHTML='';
+    for(const plant of plants){
+        const newDiv = document.createElement('div')
+        newDiv.innerHTML = `
+                            <div class="card bg-white p-4 text-start space-y-3">
+                            <img class="mx-auto rounded-lg  aspect-3/2 object-cover" src="${plant.image}" alt="Card Image">
+                            <h1 onclick="cardDetails(${plant.id})" class="font-bold text-start hover:text-green-600 cursor-pointer">${plant.name}</h1>
+                            <p class="opacity-80 text-[12px]">${plant.description}</p>
+                            <div class="price">
+                            <div class="type flex justify-between">
+                                <p class="py-1 px-3 font-semibold bg-[#DCFCE7] text-[#15803D] rounded-full">${plant.category}</p>
+                                <p class="font-semibold"><i class="fa-solid fa-bangladeshi-taka-sign"></i><span>${plant.price}</span></p>
+                            </div>
+                            </div>
+                            <div class="addToCart">
+                                <button onclick="my_modal_2.showModal()" class="bg-[#15803D] btn font-bold text-center rounded-full w-full  text-white">Add To Cart</button>
+                            </div>
+                            </div>
+        `;
+        cardContainer.append(newDiv);
+        spinner.classList.add('hidden');
+    }
+
+}
+// defaultPlants();
